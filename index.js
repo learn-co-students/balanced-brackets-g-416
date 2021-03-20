be0a604f38774724357be4e7e1d546e3f7c98838
Wendy Harris <wendyv510@hotmail.com>
2021-03-20 12:35:13 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let isBalanced = (input) => {
+
+    let brackets = "[]{}()<>"
+    let stack = []
+  
+    for(let bracket of input) {
+      let bracketsIndex = brackets.indexOf(bracket)
+  
+      if(bracketsIndex % 2 === 0) {
+        stack.push(bracketsIndex + 1)
+      } else {
+        if(stack.pop() !== bracketsIndex) {
+          return false;
+        }
+      }
+    }
+    return stack.length === 0
+  }
